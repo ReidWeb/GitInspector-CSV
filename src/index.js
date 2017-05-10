fe5d0d0e1548b9259a5f010977ee4ae040dca8cf
Peter Reid <peter@reidweb.com>
2017-05-10 15:03:30 +0100

    Add error handling to xml.js

--- src/index.js
+++ src/index.js
@@ -17,10 +17,22 @@ program
 if (program.input && program.output) {
     console.log("Starting parse");
     json = xml.parse(program.input).then(function (responsibilities) {
+
         csv.generate(responsibilities, program.output).then(function (csv) {
             fs.writeFile(program.output, csv, 'utf8');
             console.log("Done processing");
         });
+
+    }).catch(function (error) {
+        if (error.code === "ENOENT") {
+            console.error("Error: No such file or directory " + program.input);
+        } else if (error.code === "BADFORMAT") {
+            console.error(error.message);
+        } else {
+            console.error("A critical error has been encountered reading the file: ");
+            console.error(error.stack);
+        }
+        process.exit(1);
     });
 } else {
     program.outputHelp();
--- src/xml.js
+++ src/xml.js
@@ -2,17 +2,24 @@
 
 let Promise = require("bluebird");
 let cheerio=require('cheerio');
-let fs = Promise.promisifyAll(require("fs"));
+let fs = Promise.promisifyAll(require('fs'));
 let parser = require('xml2json');
 
 function parse(input) {
-    return new Promise(function( resolve, object)
+    return new Promise(function (resolve, reject)
     {
         fs.readFileAsync(input, "utf8").then(function (contents) {
             let result = JSON.parse(parser.toJson(contents));
-            resolve(result.gitinspector.responsibilities);
+            if (result.gitinspector.responsibilities) {
+                resolve(result.gitinspector.responsibilities);
+            } else {
+                let error = new Error("Error: Responsbility output is missing from passed in gitinspector XML " +
+                    "output, please verify that you ran gitinspector with the -r flag.");
+                error.code = "BADFORMAT";
+                reject(error);
+            }
         }).catch(function(e) {
-            console.error(e.stack);
+            reject(e);
         });
     });
 }
