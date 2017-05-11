3f0d7a47fb5a3d60c5f96170c57762ab6c79fdda
Peter Reid <peter@reidweb.com>
2017-05-11 18:41:48 +0100

    Refactor tests

--- test/testCsv.js
+++ test/testCsv.js
@@ -5,17 +5,21 @@ let should = chai.should();
 let rewire = require('rewire');
 let csv = rewire('../src/csv');
 
+function setupFileTestArray() {
+    let testArr = [];
+    for (let i = 0; i < 72; i++) {
+        let testObj = {};
+        testObj.name = 'file' + i + '.java';
+        testObj.extraProperty = 'blah';
+        testArr.push(testObj);
+    }
+    return testArr;
+}
 describe('Searching for a matching file name in the array of file names', function () {
-    it('should resolve the index of the corresponding filename', function () {
+    it('should resolve the index of the corresponding filename when the matching file of same name is found', function () {
         let fn = csv.__get__('searchForMatchingFile');
         //Setup test array
-        let testArr = [];
-        for (let i = 0; i < 72; i++) {
-            let testObj = {};
-            testObj.name = 'file' + i + '.java';
-            testObj.extraProperty = 'blah';
-            testArr.push(testObj);
-        }
+        let testArr = setupFileTestArray();
         (fn('file29.java', testArr)).then(function (result) {
             result.should.equal(29);
         });
@@ -26,13 +30,7 @@ describe('Searching for a matching file name in the array of file names', functi
     it('should reject with appropriate error when a matching file cannot be found', function () {
         let fn = csv.__get__('searchForMatchingFile');
         //Setup test array
-        let testArr = [];
-        for (let i = 0; i < 72; i++) {
-            let testObj = {};
-            testObj.name = 'file' + i + '.java';
-            testObj.extraProperty = 'blah';
-            testArr.push(testObj);
-        }
+        let testArr = setupFileTestArray();
         (fn('file72.java', testArr)).catch(function (err) {
             err.code.should.equal('FILENOTFOUND');
         });
